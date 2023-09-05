09cd8a4513b921d8c5dba67e224cb7584315af78
Egor Kirienko <egor.keen@mail.ru>
2023-09-05 19:26:39 +0300

    feat: улучшение карточек: верстка

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -5,13 +5,19 @@ function Todo({ todo, key, task, onDeleteButtonClick }) {
 
   const todoDoneButtonClass = `todo__done-button ${isDone ? 'todo__done-button_active' : ''}`;
   const todoTaskClass = `todo__task ${isDone ? 'todo__task_done' : ''}`;
-
+  const content = 'English texts for beginners to practice reading and comprehension online and for free. Practicing your comprehension of written English will both improve your vocabulary and understanding of grammar and word order. The texts below are designed to help you develop while giving you an instant evaluation of your progress';
+  
   function handleDoneClick() {
+    todo.isDone = !isDone;
     if (isDone) {
       setDone(false);
     } else {
       setDone(true);
     }
+  }
+
+  function handleEditClick() {
+    // some function
   }
 
   function handleDeleteClick() {
@@ -20,9 +26,23 @@ function Todo({ todo, key, task, onDeleteButtonClick }) {
 
   return (
     <article className="todo">
-      <button className={todoDoneButtonClass} onClick={handleDoneClick} />
-      <h3 className={todoTaskClass}>{task}</h3>
-      <button className="todo__delete-button" onClick={handleDeleteClick} />
+        <h3 className={todoTaskClass}>{task}</h3>
+        <button className="todo__delete-button" onClick={handleDeleteClick} />
+        <span className="todo__deadline">До 05.06.2024 8:25 AM</span>
+        <p />
+          <p className="todo__description">{content}</p>
+          <p />
+      {
+        isDone ? 
+        <>
+          <button className={todoDoneButtonClass} onClick={handleDoneClick} />
+        </>
+        :
+        <>
+          <button className={todoDoneButtonClass} onClick={handleDoneClick}>Выполнено</button>
+          <button className="todo__edit-button" onClick={handleEditClick} />
+        </>
+      }
     </article>
   );
 };
