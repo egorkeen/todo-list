04ee92c57a4815b2752d8951cf49b2611e9472f5
Egor Kirienko <egor.keen@mail.ru>
2023-11-02 20:16:15 +0300

    feat: added some basic redux structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './components/App/App';
-import { HashRouter } from "react-router-dom"
+import { store, persistor } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </PersistGate>
+  </Provider>
 );
 
