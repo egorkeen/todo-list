7a11f6b17e8893c1c8dcdc876d157c166c1507d3
Egor Kirienko <egor.keen@mail.ru>
2023-11-02 20:21:47 +0300

    feat: added todos actions

---
+++ src/store/todos/todos-actions.js
@@ -0,0 +1,32 @@
+export const ADD_TODO = '@@todos/ADD_TODO';
+export const REMOVE_TODO = '@@todos/REMOVE_TODO';
+export const EDIT_TODO = '@todos/EDIT_TODO';
+export const COMPLETE_TODO = '@todos/COMPLETE_TODO';
+
+export const addTodo = (todo) => {
+  return {
+    type: ADD_TODO,
+    payload: todo,
+  }
+};
+
+export const removeTodo = (todo) => {
+  return {
+    type: REMOVE_TODO,
+    payload: todo,
+  }
+};
+
+export const editTodo = (todo) => {
+  return {
+    type: EDIT_TODO,
+    payload: todo,
+  }
+};
+
+export const completeTodo = (todo) => {
+  return { 
+    type: completeTodo,
+    payload: todo,
+  }
+};
